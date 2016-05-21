6dd313f57f9e1933fa3dab47f55a5fa92e4cfac2
PresdentOfSky <Trunovaleksei@gmail.com>
2016-05-21 17:58:10 +0700

    second commit

--- core/src/com/mygdx/game/gameObjects/Mario.java
+++ core/src/com/mygdx/game/gameObjects/Mario.java
@@ -10,8 +10,38 @@ public class Mario {
     private Vector2 velocity;
 
     private float rotation;
-    private int weight;
+    private int width;
     private int height;
 
-    public Mario()
+    public Mario(float x, float y, int width, int height) {
+        this.width = width;
+        this.height = height;
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 0);
+
+    }
+
+    public void onClick(){
+        velocity.y = -140;
+    }
+
+    public float getX() {
+        return position.x;
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getRotation() {
+        return rotation;
+    }
 }
